d8f1b4625ad930103db50f1c04c92402b3159a7b
Alexandru Berce <alexandru.berce@gmail.com>
2017-08-20 03:44:17 +0300

    Install WP from command line and add plugin

---
+++ config/docker/wpcli/scripts/install_wp.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sleep 30 #the only hack i've found to make this work
+cd /var/www/html
+wp core install --url="127.0.0.1:8080" --title="WordPress" --admin_user="admin" --admin_password="admin" --admin_email="admin@email.com"
+wp plugin install captainform --activate
